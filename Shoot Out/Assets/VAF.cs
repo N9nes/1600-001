a75ecd77765f930232bc51d461f7f00463642858
Alan Alvarez <ajalvarez97@gmail.com>
2016-01-20 08:16:05 -0700

    Syntax assignment 1/20/16

---
+++ Shoot Out/Assets/VAF.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class VAF : MonoBehaviour 
+{
+	public int Pizza = 4000000;
+
+	// Use this for initialization
+	void Update () 
+	{
+		Pizza = DivideByTwo(Pizza);
+		Debug.Log (Pizza);
+	}
+
+	int DivideByTwo (int number)
+	{
+		int ret;
+		ret = number / 8;
+		return ret;
+	}
+}
